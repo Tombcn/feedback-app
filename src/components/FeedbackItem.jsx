8ac1d9aeb0254351c468a6b9d8cac7c43a44f518
TomBcn <tbvanreijn@gmail.com>
2022-03-18 14:35:57 +0100

    commit

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,20 @@
+import Card from "./shared/Card";
+import PropTypes from 'prop-types'
+function FeedbackItem({item}) {
+  
+  
+  return(
+  <Card>
+    <div className="num-display">{item.rating}</div>
+    <div className="text-display">{item.text}</div>
+    </Card>
+  )
+
+  Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool,
+  }
+  
+}
+
+export default FeedbackItem;
