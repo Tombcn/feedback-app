43cc1e13953b66c7520bf1355e42e9aa9e67b22a
TomBcn <tbvanreijn@gmail.com>
2022-03-24 17:17:43 +0100

    Hi

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,29 +1,22 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from "./FeedbackItem"
+import { FaTimes } from "react-icons/fa";
+import { useContext } from "react";
+import FeedbackItem from "./FeedbackItem";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
 
   return (
-    <div className='feedback-list'>
-        {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} />
-            ))}
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
     </div>
   );
-}
-
-FeedbackList.propTypes={
-  feedback: PropTypes.arrayOff(
-    PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-
-})
-  ),
 }
 
 export default FeedbackList;
