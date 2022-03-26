f11ca2a9d2bcb3fbfc3169f7eac974fd9559f5d9
TomBcn <tbvanreijn@gmail.com>
2022-03-26 10:34:26 +0100

    latest

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,19 +1,18 @@
-import { useState, useContext, useEffect } from "react";
-import Card from "./shared/Card";
-import Button from "./shared/Button";
-import Ratingselect from "./Ratingselect";
-import FeedbackContext from "../context/FeedbackContext";
-
+import { useState, useContext, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackForm() {
-  const [text, setText] = useState("");
-  const [rating, setRating] = useState(10);
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState("");
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const { addFeedback, feedbackEdit, updateFeedback } =
-  useContext(FeedbackContext)
-  
+    useContext(FeedbackContext)
+
   useEffect(() => {
     if (feedbackEdit.edit === true) {
       setBtnDisabled(false)
@@ -22,29 +21,32 @@ function FeedbackForm() {
     }
   }, [feedbackEdit])
 
-  const handleTextChange = ({ target: { value } }) => {
-    // 👈  get the value
-    if (value === "") {
-      setBtnDisabled(true);
-      setMessage(null);
-    } else if (value !== "" && value.trim().length < 10) {
-      // 👈 check for less than
-      setMessage("Text must be at least 10 characters");
-      setBtnDisabled(true);
+  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
+
+  // prettier-ignore
+  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      
+  // prettier-ignore
+    } else if (value.trim().length < 10) { // 👈 check for less than 10
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
     } else {
-      setMessage(null);
-      setBtnDisabled(false);
+      setMessage(null)
+      setBtnDisabled(false)
     }
-    setText(value);
-  };
+    setText(value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (text.trim().length > 10) {
       const newFeedback = {
         text,
         rating,
-      };
+      }
 
       if (feedbackEdit.edit === true) {
         updateFeedback(feedbackEdit.item.id, newFeedback)
@@ -52,33 +54,35 @@ function FeedbackForm() {
         addFeedback(newFeedback)
       }
 
-      addFeedback(newFeedback);
-      
-      setText("");
+      // NOTE: reset to default state after submission
+      setBtnDisabled(true) // 👈  add this line to reset disabled
+      setRating(10) //👈 add this line to set rating back to 10
+      setText('')
     }
-  };
+  }
 
+  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <Ratingselect select={(rating) => setRating(rating)} />
-        <div className="input-group">
+        <RatingSelect select={setRating} selected={rating} />
+        <div className='input-group'>
           <input
             onChange={handleTextChange}
-            type="text"
-            placeholder="Write a review"
+            type='text'
+            placeholder='Write a review'
             value={text}
           />
-
-          <Button type="submit" isDisabled={btnDisabled}>
+          <Button type='submit' isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
-        {message && <div className="message">{message}</div>}
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
-  );
+  )
 }
 
-export default FeedbackForm;
+export default FeedbackForm
