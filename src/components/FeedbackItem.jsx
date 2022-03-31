51de07e02c9fd0de5b7f31401fa2c4b0492d3459
Tombcn <tvanreijn@icloud.com>
2022-03-31 09:16:36 +0200

    Update FeedbackItem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,27 +1,28 @@
-import Card from "./shared/Card";
-import { useContext } from "react";
-import { FaTimes, FaEdit } from "react-icons/fa";
-import FeedbackContext from "../context/FeedbackContext";
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({item}) {
-  const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
-  
-  
-  return(
-  <Card>
-    <div className="num-display">{item.rating}</div>
-    <button onClick={() => deleteFeedback(item.id)} className='close'>
-      <FaTimes color='purple'/>
-    </button>
-    <button onClick={() => editFeedback(item)} className='edit'>
-      <FaEdit color='purple' />
-    </button>
-    <div className="text-display">{item.text}</div>
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
     </Card>
   )
-
-  
-  
 }
 
-export default FeedbackItem;
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
